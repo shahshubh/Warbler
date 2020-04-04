1698e8a5659a06388d759534e93c1c2990fca408
shubh <shahshubh1010@gmail.com>
2020-04-04 19:42:37 +0530

    added user model

---
+++ warbler-server/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImageUrl: {
+        type: String
+    }
+});
+
+userSchema.pre("save", async function(){  //Pre i.e. before we save this function will run
+    try{
+        if(!this.isModified("password")){
+            return next();
+        } 
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+userSchema.method.comparePassword = async function(candidatePassword, next){
+    try {
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (error) {
+        next(error);
+    }
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
