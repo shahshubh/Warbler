da251562a6e32665871d1bde3db805c7b0bd8b7c
shubh <shahshubh1010@gmail.com>
2020-04-05 15:21:42 +0530

    added middleware to creating a message

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -8,6 +8,8 @@ const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
 const messagesRoutes = require("./routes/messages");
 
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth")
+
 const PORT = 8081;
 
 app.use(cors());
@@ -16,7 +18,7 @@ app.use(bodyParser.json());
 //all my routes here
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users/:id/messages", messagesRoutes);
+app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
 
 
 //none routes match
