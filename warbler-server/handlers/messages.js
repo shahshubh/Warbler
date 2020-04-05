23931ab18058489f371958adc78e1463a8473ebe
shubh <shahshubh1010@gmail.com>
2020-04-05 15:49:15 +0530

    added messages routes

--- warbler-server/handlers/messages.js
+++ warbler-server/handlers/messages.js
@@ -19,10 +19,23 @@ exports.createMessage = async function(req,res,next){
     }
 }
 
+//GET - /api/users/:id/messages/:message_id
 exports.getMessage = async function(req,res,next){
-    
+    try {
+        let message = await db.Message.findById(req.params.message_id);
+        return res.status(200).json(message);
+    } catch (error) {
+        return next(error);
+    }
 }
 
+//DELETE - /api/users/:id/messages/:message_id
 exports.deleteMessage = async function(req,res,next){
-    
+    try {
+        let foundMessage = await db.Message.findById(req.params.message_id);
+        await foundMessage.remove();
+        return res.status(200).json(foundMessage);
+    } catch (error) {
+        return next(error);
+    }
 }
--- warbler-server/index.js
+++ warbler-server/index.js
@@ -20,6 +20,20 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
 
+app.get("/api/messages", loginRequired, async function(req, res, next){
+    try {
+        let messages = await db.Messages.find()
+            .sort({createdAt: "desc"})
+            .populate("user", {
+                username: true,
+                profileImageUrl: true
+            });
+            return res.status(200).json(messages);
+    } catch (error) {
+        return next(error);
+    }
+});
+
 
 //none routes match
 app.use(function(req,res,next){
